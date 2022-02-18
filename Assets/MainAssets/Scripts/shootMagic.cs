bd2ca7d3ed3353e1792885d4e269fa6e8ce4d261
colbytaylor263 <71579568+colbytaylor263@users.noreply.github.com>
2022-02-18 00:23:35 -0600

    Added a couple more assets and the player
    player prefab has the camera controller and everything needed to move the player around in the world with a couple of attacks set up right now
    does not switch between states completely correct yet, gonna get to that later

---
+++ Assets/MainAssets/Scripts/shootMagic.cs
@@ -0,0 +1,53 @@
+﻿namespace GameCreator.Core
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	[AddComponentMenu("")]
+	
+	public class shootMagic : IAction
+	{
+		public Camera cam;
+		public Transform leftHand;
+		public float projectileSpeed = 30;
+
+		private Vector3 destination;
+
+
+		public GameObject projectile;
+		
+		public int example = 0;
+
+        public override bool InstantExecute(GameObject target, IAction[] actions, int index)
+        {
+			float screenX = Screen.width / 2;
+			float screenY = Screen.height / 2;
+			Ray ray = cam.ViewportPointToRay(new Vector3(screenX, screenY, 0));
+			RaycastHit HitInfo;
+			//check for a hit
+			if (Physics.Raycast(ray, out HitInfo))
+			{
+				destination = HitInfo.point;
+			}
+
+            else
+            {
+				destination = ray.GetPoint(1000);
+            }
+
+          
+			var projectileObj = Instantiate(projectile, leftHand.position, Quaternion.identity) as GameObject;
+
+			projectileObj.GetComponent<Rigidbody>().velocity = ((destination-leftHand.position).normalized * projectileSpeed);
+	
+
+			return true;
+        }
+
+		#if UNITY_EDITOR
+        public static new string NAME = "Custom/shootMagic";
+		#endif
+	}
+}
