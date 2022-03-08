054e83838c0422b33af6e060666fe05694d8e57d
Stevejoe80 <70162562+Stevejoe80@users.noreply.github.com>
2022-03-08 09:05:01 -0800

    Dialogfue
    Dialogue

--- Assets/MainAssets/Scripts/DialogueManager.cs
+++ Assets/MainAssets/Scripts/DialogueManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine UI;
+
+
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
     private Queue<string> sentences;
 
     // Start is called before the first frame update
@@ -14,7 +19,30 @@ public class DialogueManager : MonoBehaviour
     }
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        nameText.text = dialogue.name;
+
+
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    public void EndDialogue()
+    {
+        Debug.Log("End of Conversation. ");
     }
 
 
